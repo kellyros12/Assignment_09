ba1842bf25b7c9818cfa2d89809494360b067c3a
kellyros12 <kellyros91@gmail.com>
2020-03-22 20:54:42 -0700

    Add files via upload

---
+++ ProcessingClasses.py
@@ -0,0 +1,87 @@
+#------------------------------------------#
+# Title: Processing Classes
+# Desc: A Module for processing Classes
+# Change Log: (Who, When, What)
+# DBiesinger, 2030-Jan-01, Created File
+# DBiesinger, 2030-Jan-02, Extended functionality to add tracks
+# KRos, 2020-Mar-22, Completed ToDos
+#------------------------------------------#
+
+if __name__ == '__main__':
+    raise Exception('This file is not meant to ran by itself')
+
+import DataClasses as DC
+
+class DataProcessor:
+    """Processing the data in the application"""
+    @staticmethod
+    def add_CD(CDInfo, table):
+        """function to add CD info in CDinfo to the inventory table.
+
+
+        Args:
+            CDInfo (tuple): Holds information (ID, CD Title, CD Artist) to be added to inventory.
+            table (list of dict): 2D data structure (list of dicts) that holds the data during runtime.
+
+        Returns:
+            None.
+
+        """
+
+        cdId, title, artist = CDInfo
+        try:
+            cdId = int(cdId)
+        except:
+            raise Exception('ID must be an Integer!')
+        row = DC.CD(cdId, title, artist)
+        table.append(row)
+
+    @staticmethod
+    def select_cd(table: list, cd_idx: int) -> DC.CD:
+        """selects a CD object out of table that has the ID cd_idx
+
+        Args:
+            table (list): Inventory list of CD objects.
+            cd_idx (int): id of CD object to return
+
+        Raises:
+            Exception: If id is not in list.
+
+        Returns:
+            row (DC.CD): CD object that matches cd_idx
+
+        """
+        CDFound = False
+        for row in table:
+            if row.cd_id == int(cd_idx):
+                CDFound = True
+                return row
+        if not CDFound:
+            print('CD not found.')
+
+
+    @staticmethod
+    def add_track(track_info: tuple, cd: DC.CD) -> None:
+        """adds a Track object with attributes in track_info to cd
+
+
+        Args:
+            track_info (tuple): Tuple containing track info (position, title, Length).
+            cd (DC.CD): cd object the tarck gets added to.
+
+        Except:
+            prints Exception, and will not add track if exception is raised
+
+        Returns:
+            None: DESCRIPTION.
+
+        """
+
+
+        newPos, newTtl, newLnth = track_info
+        try:
+            newPos = int(newPos)
+            track = DC.Track(newPos, newTtl, newLnth)
+            cd.add_track(track)
+        except Exception as e:
+            print(e)
